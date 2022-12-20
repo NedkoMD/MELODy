422079a0476c81d295920f2b0c2cca89ea0cf61c
nedkom21 <nedkom21@gmail.com>
2022-12-20 15:07:31 +0200

    New project to commit to GitHub

---
+++ Models/SongGenreViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MusicApp.Models
+{
+    public class SongGenreViewModel
+    {
+        public List<Song>? Songs { get; set; }
+        public SelectList? Genres { get; set; }
+        public string? SongGenre { get; set; }
+        public string? SearchString { get; set; }
+    }
+}
